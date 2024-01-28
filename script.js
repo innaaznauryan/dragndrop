27f53a419f362ab24fb180110b1da47c8e2a1575
innaaznauryan <inna.aznauryan@gmail.com>
2024-01-28 22:54:38 +0400

    refactoring

--- script.js
+++ script.js
@@ -1,62 +1,59 @@
-const main = document.getElementById("main")
-const boards = main.querySelectorAll(".board")
+const main = document.querySelector("main")
 const emptyItem = document.createElement("div")
 emptyItem.classList.add("emptyItem")
 
 let draggableItemId = null
 
 function renderBoards() {
-    Array.from(boards).forEach(board => {
-        const items = board.children
+    main.addEventListener("dragstart", e => {
+        if(e.target.classList.contains("item")) {
+            draggableItemId = e.target.id
+            e.dataTransfer.effectAllowed = "move"
+        }
+    })
 
-        board.addEventListener("dragover", e => {
-            if(e.target.classList.contains("board") && 
-            (!e.target.children.length || e.clientY > e.target.lastElementChild.getBoundingClientRect().bottom) || 
-            e.target.classList.contains("emptyItem")) {
-                e.preventDefault()
-            }
-        })
+    main.addEventListener("dragover", e => {
+        e.preventDefault()
+        if(e.target.classList.contains("item")) {
+            e.dataTransfer.dropEffect = "none"
+            const itemStyle = e.target.getBoundingClientRect()
+            e.clientY < itemStyle.y + itemStyle.height / 2 ? e.target.insertAdjacentElement("beforebegin", emptyItem) : e.target.insertAdjacentElement("afterend", emptyItem)
+        }
+    })
 
-        Array.from(items).forEach(item => {
-            item.addEventListener("dragstart", e => {
-                draggableItemId = e.target.id
-                e.dataTransfer.effectAllowed = "move"
-            })
-        })
-
-        board.addEventListener("dragleave", e => {
-            if(e.target.classList.contains("item")) {
-                const itemStyle = e.target.getBoundingClientRect()
-                e.clientY < itemStyle.y + itemStyle.height / 2 ? e.target.insertAdjacentElement("beforebegin", emptyItem) : e.target.insertAdjacentElement("afterend", emptyItem)
-            }
-        })
-
-        board.addEventListener("dragenter", e => {
-            if(e.target.classList.contains("board") && (!e.target.children.length || e.clientY > e.target.lastElementChild.getBoundingClientRect().y)) {
+    main.addEventListener("dragenter", e => {
+        if(e.target.classList.contains("emptyItem")) {
+            emptyItem.style.border = "1px solid gray"
+        }
+        if(e.target.classList.contains("board")) {
+            if(!e.target.children.length || e.clientY > e.target.lastElementChild.getBoundingClientRect().y) {
                 e.target.appendChild(emptyItem)
-            } else if(e.target.classList.contains("emptyItem")) {
-                emptyItem.style.border = "1px solid gray"
+            } else {
+                e.dataTransfer.dropEffect = "none"
             }
-        })
+        }
+    })
 
-        board.addEventListener("drop", e => {
-            const draggableItem = main.querySelector("#" + draggableItemId)
-            if(e.target.classList.contains("board") && 
-            (!e.target.children.length || e.clientY > e.target.lastElementChild.getBoundingClientRect().y) || 
-            e.target.classList.contains("emptyItem")) {
-                board.replaceChild(draggableItem, emptyItem)
-            }
-            const sound = new Audio("./audio/dropSound.mp3")
+    main.addEventListener("drop", e => {
+        const draggableItem = main.querySelector("#" + draggableItemId)
+        if(
+            e.target.classList.contains("board") && 
+            (!e.target.children.length || e.clientY > e.target.lastElementChild.getBoundingClientRect().y)
+        ) {
+            e.target.replaceChild(draggableItem, emptyItem)
+        } else if(e.target.classList.contains("emptyItem")) {
+            e.target.replaceWith(draggableItem)
+        }
+        const sound = new Audio("./audio/dropSound.mp3")
+        sound.play()
+    })
+
+    main.addEventListener("dragend", e => {
+        if(e.dataTransfer.dropEffect === "none") {
+            emptyItem.remove()
+            const sound = new Audio("./audio/failSound.mp3")
             sound.play()
-        })
-
-        Array.from(items).forEach(item => {
-            item.addEventListener("dragend", e => {
-                if(e.dataTransfer.dropEffect === "none") {
-                    emptyItem.remove()
-                }
-            })
-        })
+        }
     })
 }
 
