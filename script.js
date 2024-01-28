3d80381a5b8c97a4d1aeffb7a925d7fae0bb3c6c
innaaznauryan <inna.aznauryan@gmail.com>
2024-01-28 13:53:19 +0400

    finalize

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ const emptyItem = document.createElement("div")
 emptyItem.classList.add("emptyItem")
 
 let draggableItemId = null
-let sourceBoardId = null
 
 function renderBoards() {
     Array.from(boards).forEach(board => {
         const items = board.children
 
         board.addEventListener("dragover", e => {
-            if(e.target.classList.contains("emptyItem") || e.target.classList.contains("board")) {
+            if(e.target.classList.contains("board") && 
+            (!e.target.children.length || e.clientY > e.target.lastElementChild.getBoundingClientRect().bottom) || 
+            e.target.classList.contains("emptyItem")) {
                 e.preventDefault()
             }
         })
@@ -19,49 +20,41 @@ function renderBoards() {
         Array.from(items).forEach(item => {
             item.addEventListener("dragstart", e => {
                 draggableItemId = e.target.id
-                sourceBoardId = e.target.parentElement.id
+                e.dataTransfer.effectAllowed = "move"
             })
         })
-        
+
         board.addEventListener("dragleave", e => {
             if(e.target.classList.contains("item")) {
                 const itemStyle = e.target.getBoundingClientRect()
                 e.clientY < itemStyle.y + itemStyle.height / 2 ? e.target.insertAdjacentElement("beforebegin", emptyItem) : e.target.insertAdjacentElement("afterend", emptyItem)
             }
-            if(e.target.classList.contains("board") && e.clientY > board.lastElementChild.getBoundingClientRect().y && board.contains(emptyItem)) {
-                e.target.removeChild(emptyItem)
+        })
+
+        board.addEventListener("dragenter", e => {
+            if(e.target.classList.contains("board") && (!e.target.children.length || e.clientY > e.target.lastElementChild.getBoundingClientRect().y)) {
+                e.target.appendChild(emptyItem)
+            } else if(e.target.classList.contains("emptyItem")) {
+                emptyItem.style.border = "1px solid gray"
             }
         })
 
         board.addEventListener("drop", e => {
             const draggableItem = main.querySelector("#" + draggableItemId)
-            if(e.target.classList.contains("board") && e.clientY > e.target.lastElementChild.getBoundingClientRect().bottom) {
-                e.target.appendChild(draggableItem)
-        
-            } else {
+            if(e.target.classList.contains("board") && 
+            (!e.target.children.length || e.clientY > e.target.lastElementChild.getBoundingClientRect().y) || 
+            e.target.classList.contains("emptyItem")) {
                 board.replaceChild(draggableItem, emptyItem)
             }
-            if(board.contains(emptyItem)) {
-                board.removeChild(emptyItem)
-            }
-            console.log(board)
+            const sound = new Audio("./audio/dropSound.mp3")
+            sound.play()
         })
 
         Array.from(items).forEach(item => {
             item.addEventListener("dragend", e => {
-                console.log(board, main.querySelector("#" + sourceBoardId))
-
                 if(e.dataTransfer.dropEffect === "none") {
-                    board.removeChild(emptyItem)
-                    main.querySelector("#" + sourceBoardId).removeChild(emptyItem)
-                    // if(board.contains(emptyItem)) {
-                    //     board.removeChild(emptyItem)
-                    // }
-                    // if(main.querySelector("#" + sourceBoardId).contains(emptyItem)) {
-                    //     main.querySelector("#" + sourceBoardId).removeChild(emptyItem)
-                    // }
-                } 
-                console.log(board)
+                    emptyItem.remove()
+                }
             })
         })
     })
