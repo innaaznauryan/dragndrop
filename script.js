503401d6d1b711f12b5b43d94b9dfc92792b12a8
innaaznauryan <inna.aznauryan@gmail.com>
2024-02-13 16:14:39 +0400

    refactoring

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let draggableItemId = null
 
 function renderBoards() {
     main.addEventListener("dragstart", e => {
-        if(e.target.classList.contains("item")) {
+        if (e.target.classList.contains("item")) {
             draggableItemId = e.target.id
             e.dataTransfer.effectAllowed = "move"
         }
@@ -14,42 +14,38 @@ function renderBoards() {
 
     main.addEventListener("dragover", e => {
         e.preventDefault()
-        if(e.target.classList.contains("item")) {
-            e.dataTransfer.dropEffect = "none"
+        if (e.target.classList.contains("item")) {
             const itemStyle = e.target.getBoundingClientRect()
             e.clientY < itemStyle.y + itemStyle.height / 2 ? e.target.insertAdjacentElement("beforebegin", emptyItem) : e.target.insertAdjacentElement("afterend", emptyItem)
         }
-    })
-
-    main.addEventListener("dragenter", e => {
-        if(e.target.classList.contains("emptyItem")) {
-            emptyItem.style.border = "1px solid gray"
+        if (e.target.classList.contains("board") && (!e.target.children.length || e.clientY > e.target.lastElementChild.getBoundingClientRect().y)) {
+            e.target.appendChild(emptyItem)
         }
-        if(e.target.classList.contains("board")) {
-            if(!e.target.children.length || e.clientY > e.target.lastElementChild.getBoundingClientRect().y) {
-                e.target.appendChild(emptyItem)
-            } else {
-                e.dataTransfer.dropEffect = "none"
-            }
+        if (e.target.id === "main") {
+            e.dataTransfer.dropEffect = "none"
+            emptyItem.remove()
         }
     })
 
     main.addEventListener("drop", e => {
         const draggableItem = main.querySelector("#" + draggableItemId)
-        if(
-            e.target.classList.contains("board") && 
-            (!e.target.children.length || e.clientY > e.target.lastElementChild.getBoundingClientRect().y)
-        ) {
+        if (e.target.classList.contains("board")) {
             e.target.replaceChild(draggableItem, emptyItem)
-        } else if(e.target.classList.contains("emptyItem")) {
+        }
+        if (e.target.classList.contains("emptyItem")) {
             e.target.replaceWith(draggableItem)
         }
+        if (e.target.classList.contains("item")) {
+            const itemStyle = e.target.getBoundingClientRect()
+            e.clientY < itemStyle.y + itemStyle.height / 2 ? e.target.insertAdjacentElement("beforebegin", draggableItem) : e.target.insertAdjacentElement("afterend", draggableItem)
+        }
+        emptyItem.remove()
         const sound = new Audio("./audio/dropSound.mp3")
         sound.play()
     })
 
     main.addEventListener("dragend", e => {
-        if(e.dataTransfer.dropEffect === "none") {
+        if (e.dataTransfer.dropEffect === "none") {
             emptyItem.remove()
             const sound = new Audio("./audio/failSound.mp3")
             sound.play()
